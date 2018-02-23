9c6ab28fb9cbc88194236f7874bc64592412dd32
Marina Kosovyagina <kosovyagina91@mail.ru>
2018-02-24 00:38:44 +0300

    Realization of Fibonacci sequence. Added some comments.

--- Однопроходные алгоритмы/Fibonacchi sequense.py
+++ Однопроходные алгоритмы/Fibonacchi sequense.py
@@ -1,14 +1,14 @@
 import sys
 if sys.version_info[0] < 3:
     input = raw_input
-s = 0
-curr = 1
-prev = 1
+s = 0 #Variable for calculating number
+curr = 1 #First number of sequence
+prev = 1 #Second number of aequence
 print("Enter the amount of first Fibonacci numbers you'd like to output")
-n = int(input())
+n = int(input())  #Length of Fibonacchi sequence
 print prev
 print curr
-for i in range(1, n-2):
+for i in range(1, n-2): #Loop where we calculate the next number
     s = prev + curr
     prev = curr
     curr = s
