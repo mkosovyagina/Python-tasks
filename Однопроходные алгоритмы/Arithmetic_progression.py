954fae7d3c3abb28dda6f9f3fc6aacab10282013
Marina Kosovyagina <kosovyagina91@mail.ru>
2018-03-04 20:07:45 +0300

    Added an algorithm how to check that the sequence is arithmetic progression or not.

--- Однопроходные алгоритмы/Arithmetic_progression.py
+++ Однопроходные алгоритмы/Arithmetic_progression.py
@@ -5,11 +5,43 @@ if sys.version_info[0] < 3:
     input = raw_input
 
 directory = "."
-filename = input("Enter filename")
+filename = "Arithmetic_progression_2018-03-04.txt"
 thepath = path.join(directory, filename)
 file = open(thepath)
-content = file.read()
+is_data = True
+curr = ""
+number_list = []
+
+while is_data:
+    content = file.read(1)
+    if content:
+        is_data = True
+        if content == " ":
+            number = float(curr)
+            curr = ""
+            number_list.append(number)
+        else:
+            curr += content
+    else:
+        is_data = False
 print(content)
 
+n = 1
+prev = number_list[0]
+curr = number_list[1]
+diff = curr - prev
+is_progression = True
+while n < len(number_list)-1:
+    n += 1
+    prev = curr
+    curr = number_list[n]
+    if (curr - prev) != diff:
+        is_progression = False
+        break
+file.flush()
+file.close()
 
-
+if is_progression == False:
+    print("This sequence is not arithmetic progression")
+else:
+    print("This sequence is arithmetic progression")
